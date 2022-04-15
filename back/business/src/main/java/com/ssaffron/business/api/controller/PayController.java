f82cad28d56a678a41d156763bca0f0c4bed58a3
s6082648 <s6082648@naver.com>
2022-04-15 16:52:32 +0900

    Feat: create pay controller
    pay controller 생성

--- back/business/src/main/java/com/ssaffron/business/api/controller/PayController.java
+++ back/business/src/main/java/com/ssaffron/business/api/controller/PayController.java
@@ -1,4 +1,28 @@
 package com.ssaffron.business.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/api/pay")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PayController {
+
+    @PostMapping("/{payId}")
+    public ResponseEntity applicationPayment(@PathVariable("payId") int payId) { //신청
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{payId}")
+    public ResponseEntity updatePayment(@PathVariable("payId") int payId) { // 수정
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{payId}")
+    public ResponseEntity deletePayment(@PathVariable("payId") int payId) { // 삭제
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
