f24bd2229572b17365360e2003bf3fd59b8bfe8b
hwangseonju <tjswn0425@naver.com>
2022-04-15 17:00:09 +0900

    Feat: add pay controller getmapping
    add paycontroller getmapping

--- back/business/src/main/java/com/ssaffron/business/api/controller/PayController.java
+++ back/business/src/main/java/com/ssaffron/business/api/controller/PayController.java
@@ -1,4 +1,40 @@
 package com.ssaffron.business.api.controller;
 
+import com.ssaffron.business.api.dto.FeeDto;
+import com.ssaffron.business.api.dto.PaymentDto;
+import com.ssaffron.business.api.entity.FeeEntity;
+import com.ssaffron.business.api.entity.PaymentEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/api/pay")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PayController {
+
+    @GetMapping("/paylist")
+    public ResponseEntity<List<PaymentEntity>> payAllList() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/{payIdx}")
+    public ResponseEntity<PaymentEntity> payOneList(@PathVariable int payIdx) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/feelist")
+    public ResponseEntity<List<FeeEntity>> feeAllList() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/fee/{feeIdx}")
+    public ResponseEntity<FeeEntity> feeOneList(@PathVariable int feeIdx) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
