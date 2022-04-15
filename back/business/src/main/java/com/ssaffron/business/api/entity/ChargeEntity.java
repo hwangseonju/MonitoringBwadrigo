ecac0990d4581066e0cbc9d07f7f374768345b4e
SSAFY <chlj1101@gmail.com>
2022-04-15 10:33:08 +0900

    Feat: create charge jpa entity
    charge 엔티티 만들고
    application.yml 수정

--- back/business/src/main/java/com/ssaffron/business/api/entity/ChargeEntity.java
+++ back/business/src/main/java/com/ssaffron/business/api/entity/ChargeEntity.java
@@ -1,4 +1,39 @@
 package com.ssaffron.business.api.entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class ChargeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "charge_idx")
+    private long chargeIdx;
+
+    @Column(name = "request_cnt", nullable = false)
+    private long requestCnt;
+
+    @Column(name = "responsed_at")
+    private LocalDateTime responsedAt;
+
+    @Column(name = "responsed_at")
+    private LocalDateTime pickDate;
+
+//    @OneToOne(mappedBy = "optionalIdx")
+//    private OptionalEntity optionalEntity;
+//    관계를 끊어도 상관없고
+//    관계가 있어도 charge에서는 optional을 참조할 필요가 없음
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "collection_idx")
+    private CollectionEntity collectionEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx")
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fee_idx")
+    private FeeEntity feeEntity;
 }
--- back/business/src/main/java/com/ssaffron/business/api/entity/CollectionEntity.java
+++ back/business/src/main/java/com/ssaffron/business/api/entity/CollectionEntity.java
@@ -1,4 +1,11 @@
 package com.ssaffron.business.api.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CollectionEntity {
+
+    @Id
+    private long collectionIdx;
 }
