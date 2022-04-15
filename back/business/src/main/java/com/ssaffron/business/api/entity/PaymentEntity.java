de758ed76ec66b9c0697d3001e3dad82e7dc7802
hwangseonju <tjswn0425@naver.com>
2022-04-15 11:04:26 +0900

    Feat: add paymentEntity
    dd paymentEntity

--- back/business/src/main/java/com/ssaffron/business/api/entity/PaymentEntity.java
+++ back/business/src/main/java/com/ssaffron/business/api/entity/PaymentEntity.java
@@ -1,4 +1,29 @@
 package com.ssaffron.business.api.entity;
 
+import lombok.Cleanup;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payment")
 public class PaymentEntity {
+
+    @Id
+    @Column(name = "payment_idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int paymentIdx;
+    @Column(name = "payment_name", nullable = false, length = 30)
+    String paymentName;
+    @Column(name = "payment_price", nullable = false)
+    int paymentPrice;
+    @Column(name = "wash_cnt")
+    int washCnt;
+    @Column(name = "cleaning_cnt")
+    int cleaningCnt;
+    @Column(name = "shirt_cnt")
+    int shirtCnt;
+    @Column(name = "bedding_cnt")
+    int beddingCnt;
+    @Column(name = "delivery_cnt")
+    int deliveryCnt;
 }
