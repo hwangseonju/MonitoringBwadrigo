f34016ad37ca5ac0c008b2d8e8c9e696edc2fd63
SSAFY <chlj1101@gmail.com>
2022-04-15 15:11:00 +0900

    Feat: create employee dto
    EmployeeDto 생성

--- back/business/src/main/java/com/ssaffron/business/api/dto/EmployeeDto.java
+++ back/business/src/main/java/com/ssaffron/business/api/dto/EmployeeDto.java
@@ -1,4 +1,20 @@
 package com.ssaffron.business.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
+
+    private int employeeIdx;
+
+    private String employeeName;
+
+    private String employeePhone;
+
 }
