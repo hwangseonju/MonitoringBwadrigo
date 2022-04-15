2c594a1e4c9e663aeb4564c706f912faf7070846
s6082648 <s6082648@naver.com>
2022-04-15 11:23:08 +0900

    Feat: add OptionalEntity
    OptionalEntity

--- back/business/src/main/java/com/ssaffron/business/api/entity/OptionalEntity.java
+++ back/business/src/main/java/com/ssaffron/business/api/entity/OptionalEntity.java
@@ -1,4 +1,39 @@
 package com.ssaffron.business.api.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "optionalEntity")
 public class OptionalEntity {
+
+    @Id
+    @Column(name = "optional_idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int optionalIdx;
+
+    @Column(name = "wash_cnt")
+    private int washCnt;
+
+    @Column(name = "bedding_cnt")
+    private int beddingCnt;
+
+    @Column(name = "delivery_cnt")
+    private int deliveryCnt;
+
+    @Column(name = "cleaning_cnt")
+    private int cleaningCnt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx")
+    private UserEntity userIdx;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_idx")
+    private PaymentEntity paymentIdx;
+
 }
