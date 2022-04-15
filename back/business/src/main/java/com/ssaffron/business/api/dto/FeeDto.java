1c71b0d962e063dc72ab8a94021e9ffd035d171e
SSAFY <chlj1101@gmail.com>
2022-04-15 15:06:57 +0900

    Feat: create fee dto
    FeeDto 생성

--- back/business/src/main/java/com/ssaffron/business/api/dto/FeeDto.java
+++ back/business/src/main/java/com/ssaffron/business/api/dto/FeeDto.java
@@ -1,4 +1,24 @@
 package com.ssaffron.business.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FeeDto {
+
+    private int feeIdx;
+
+    private String type;
+
+    private String details;
+
+    private int price;
+
+    private String description;
+
 }
