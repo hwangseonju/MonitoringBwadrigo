d0cece6894bfc571c9f36c1e38fa18cc8bebd559
hwangseonju <tjswn0425@naver.com>
2022-04-15 15:13:36 +0900

    Feat: add payment Dto
    add payment Dto

--- back/business/src/main/java/com/ssaffron/business/api/dto/PaymentDto.java
+++ back/business/src/main/java/com/ssaffron/business/api/dto/PaymentDto.java
@@ -1,4 +1,23 @@
 package com.ssaffron.business.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDto {
+
+    private int paymentIdx;
+    private String paymentName;
+    private int paymentPrice;
+    private int washCnt;
+    private int cleaningCnt;
+    private int shirtCnt;
+    private int beddingCnt;
+    private int deliveryCnt;
+
 }
