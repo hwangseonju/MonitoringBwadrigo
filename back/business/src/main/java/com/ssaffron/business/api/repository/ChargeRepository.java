10fe888f59947aded27b5a488ac270d65b0329c9
SSAFY <chlj1101@gmail.com>
2022-04-19 13:58:59 +0900

    Feat: update entity dto repository
    멘토링 기반 수ì정

--- back/business/src/main/java/com/ssaffron/business/api/repository/ChargeRepository.java
+++ back/business/src/main/java/com/ssaffron/business/api/repository/ChargeRepository.java
@@ -1,9 +1,9 @@
 package com.ssaffron.business.api.repository;
 
-import com.ssaffron.business.api.entity.ChargeEntity;
+import com.ssaffron.business.api.entity.PayForEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChargeRepository extends JpaRepository<ChargeEntity, Long> {
+public interface ChargeRepository extends JpaRepository<PayForEntity, Long> {
 }
