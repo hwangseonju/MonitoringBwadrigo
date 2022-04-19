8fd2a187684cdfaa310b851772472b1d2e4fdd95
dodssockii <dodssockii@gmail.com>
2022-04-19 18:17:16 +0900

    Feat: update planController and create planService

--- back/business/src/main/java/com/ssaffron/business/api/controller/PlanController.java
+++ back/business/src/main/java/com/ssaffron/business/api/controller/PlanController.java
@@ -1,7 +1,7 @@
 package com.ssaffron.business.api.controller;
 
-import com.ssaffron.business.api.entity.FeeEntity;
-import com.ssaffron.business.api.entity.PaymentEntity;
+import com.ssaffron.business.api.entity.LaundryPlanEntity;
+import com.ssaffron.business.api.entity.MonthPlanEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,38 +13,45 @@ import java.util.List;
 @RequestMapping("/v1/api/pay")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-public class PayController {
+public class PlanController {
 
+    // dd
     @GetMapping("/paylist")
-    public ResponseEntity<List<PaymentEntity>> payAllList() {
+    public ResponseEntity<List<MonthPlanEntity>> payAllList() {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    // dd
     @GetMapping("/{payIdx}")
-    public ResponseEntity<PaymentEntity> payOneList(@PathVariable int payIdx) {
+    public ResponseEntity<MonthPlanEntity> payOneList(@PathVariable int payIdx) {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    // tt
     @GetMapping("/feelist")
-    public ResponseEntity<List<FeeEntity>> feeAllList() {
+    public ResponseEntity<List<LaundryPlanEntity>> feeAllList() {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    // tt
     @GetMapping("/fee/{feeIdx}")
-    public ResponseEntity<FeeEntity> feeOneList(@PathVariable int feeIdx) {
+    public ResponseEntity<LaundryPlanEntity> feeOneList(@PathVariable int feeIdx) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // tt
     @PostMapping("/{payId}")
     public ResponseEntity applicationPayment(@PathVariable("payId") int payId) { //신청
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // dd
     @PutMapping("/{payId}")
     public ResponseEntity updatePayment(@PathVariable("payId") int payId) { // 수정
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    // tt
     @DeleteMapping("/{payId}")
     public ResponseEntity deletePayment(@PathVariable("payId") int payId) { // 삭제
         return new ResponseEntity<>(HttpStatus.OK);
