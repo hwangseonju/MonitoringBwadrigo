002bb8fb7198a0be3b6958e5451b5db7808ac314
asd2456542 <asd2456542@naver.com>
2022-04-28 13:58:27 +0900

    [S06P31S104-159]Feat: update_collect_application
    수거신청,취소,상세 구현

--- front/bwadrigo/src/components/collection/Application.jsx
+++ front/bwadrigo/src/components/collection/Application.jsx
@@ -1,35 +1,52 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import getAxios from '../../Api';
 
 function Application(){
     const [checkValue, setCheckValue] = useState([]);
     const [addrress, setAddress] = useState();
     const [isAddress, setIsAddress] = useState(false);
 
-    const checkAddress = () =>{
-        axios.get("http://k6s104.p.ssafy.io:8081/v1/api/member")
-        .then((res)=>{
+    const axios = getAxios();
+    useEffect(() => {
+        try{
+            axios.get("/v1/api/member")
+            .then((res)=>{
             console.log(res);
             if(res.data.memberAddress){
                 setAddress(res.data.memberAddress);
                 setIsAddress(true);
             }
         })
-    }
+        }
+        catch(err){
+            console.log(err);
+        }
+    },[]);
     
     const changeHandler = (checked, id) => {
         if(checked){
-            setCheckValue([...checkValue, id]);
+            setCheckValue([...checkValue, {collectFortype : id}]);
             console.log(id,"체크");
         }
         else{
-            setCheckValue(checkValue.filter(button => button!==id));
+            setCheckValue(checkValue.filter(e => !e.collectFortype.includes(id)));
             console.log(id,"체크 해제");
         }
     }
     const test = () =>{
         console.log(checkValue);
+    }
+    const navigate = useNavigate();
+    const submit = async () =>{
+        try{
+            await axios.post("/v1/api/order", checkValue);
+            navigate("/applicationResult");
+        }
+        catch(err){
+            console.log(err);
+        }
     }
 
     return(
@@ -38,13 +55,13 @@ function Application(){
             <h2>수거/배송 주소</h2>
             {isAddress?
             <div>
-                <h2>입력된 주소가 없습니다.</h2>
-                <Button>주소 입력하러 가기!</Button>
+                <h2>{addrress}</h2>
+                <Button>주소 수정하러 가기!</Button>
             </div>
             :
             <div>
-                <h2>{addrress}</h2>
-                <Button>주소 수정하러 가기!</Button>
+                <h2>입력된 주소가 없습니다.</h2>
+                <Button>주소 입력하러 가기!</Button>  
             </div>  
             }
             <p>요청사항</p>
@@ -52,30 +69,30 @@ function Application(){
             <h2>맡길 세탁물 선택하기</h2>
             <Form>
                 <Form.Check type='checkbox' id='wash' label='생활빨래' 
-                checked = {checkValue.includes('wash') ? true : false} 
+                checked = {checkValue.filter((e)=>{return e.collectFortype.includes('wash')}).length>0 ? true : false} 
                 onChange={e => {
-                    changeHandler(e.currentTarget.checked,'wash');
+                    changeHandler(e.currentTarget.checked,e.currentTarget.id);
                     }} 
                 />
                 <Form.Check type='checkbox' id='bedding' label='이불' 
-                checked = {checkValue.includes('bedding') ? true : false} 
+                checked = {checkValue.filter((e)=>{return e.collectFortype.includes('bedding')}).length>0 ? true : false} 
                 onChange={e => {
                     changeHandler(e.currentTarget.checked,e.currentTarget.id);
                     }} 
                 /><Form.Check type='checkbox' id='shirts' label='와이셔츠' 
-                checked = {checkValue.includes('shirts') ? true : false} 
+                checked = {checkValue.filter((e)=>{return e.collectFortype.includes('shirts')}).length>0 ? true : false} 
                 onChange={e => {
                     changeHandler(e.currentTarget.checked,e.currentTarget.id);
                     }} 
                 /><Form.Check type='checkbox' id='cleaning' label='드라이 클리닝' 
-                checked = {checkValue.includes('cleaning') ? true : false} 
+                checked = {checkValue.filter((e)=>{return e.collectFortype.includes('cleaning')}).length>0 ? true : false} 
                 onChange={e => {
                     changeHandler(e.currentTarget.checked,e.currentTarget.id);
                     }} 
                 />
             </Form>
 
-            <Button href='/applicationResult' variant='success' size='lg'>수거신청하기</Button>
+            <Button onClick={submit} variant='success' size='lg'>수거신청하기</Button>
             <Button onClick={test}>test</Button>
         </div>
     )
