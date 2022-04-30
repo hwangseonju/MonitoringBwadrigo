0312e3bfadacc52eaa79dad3ecdcb2e02f7fa14f
doyeon <kiixdox@gmail.com>
2022-05-01 05:27:29 +0900

    Feat : make plan list in frontend
    applyplanlist, laundryplanlist, applydetail make

--- front/bwadrigo/src/App.js
+++ front/bwadrigo/src/App.js
@@ -1,13 +1,19 @@
 import Test from './test/test';
 import Home from './components/Home';
+import ApplyPlan from './components/ApplyPlan';
+import LandryPlan from './components/LaundryPlan'
+import ApplyPlanDetail from './components/ApplyPlanDetail';
 import {Route, Routes} from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/test" element={<Test/>} />
+        <Route path="/applyplan" element={<ApplyPlan/>}/>
+        <Route path="/laundryplan" element={<LandryPlan/>}/>
+        <Route path="/applydetail/:applyid" element={<ApplyPlanDetail/>}/>
       </Routes>
     </div>
   );
