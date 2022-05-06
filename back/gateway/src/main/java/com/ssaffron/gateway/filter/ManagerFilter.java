5fbc1a6e569fa83ad8a134c87e91a8a72ef016ee
hwangseonju <tjswn0425@naver.com>
2022-05-06 16:28:46 +0900

    [S06P31S104-227] Fix: add role check
    ㅇã직원 권한 확인

--- back/gateway/src/main/java/com/ssaffron/gateway/filter/ManagerFilter.java
+++ back/gateway/src/main/java/com/ssaffron/gateway/filter/ManagerFilter.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -20,19 +23,19 @@ import java.util.Objects;
 import java.util.StringTokenizer;
 
 @Component
-public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
-    private static final Logger logger = LogManager.getLogger(AuthFilter.class);
-    public AuthFilter() {
+public class ManagerFilter extends AbstractGatewayFilterFactory<ManagerFilter.Config> {
+    private static final Logger logger = LogManager.getLogger(ManagerFilter.class);
+    public ManagerFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
-            logger.info("AuthFilter baseMessage>>>>>>" + config.getBaseMessage());
+            logger.info("ManagerFilter baseMessage>>>>>>" + config.getBaseMessage());
             if (config.isPreLogger()) {
                 ServerHttpRequest request = exchange.getRequest();
-                logger.info("AuthFilter Start>>>>>>" + request);
+                logger.info("ManagerFilter Start>>>>>>" + exchange.getRequest());
 
                 // token이 있는지 없는 여부 확인
                 if(!request.getHeaders().containsKey("Cookie")) {
@@ -40,21 +43,28 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                 }
 
                 // token 가져오기
-                List<String> token = request.getHeaders().get("Cookie");
+                List<String> token = exchange.getRequest().getHeaders().get("Cookie");
                 String tokenString = Objects.requireNonNull(token).get(0);
 
-                logger.info("token 확인>>>>>>"+tokenString);
+                StringTokenizer st = new StringTokenizer(tokenString, "=,;");
+                st.nextToken();
+                String accessToken = st.nextToken();
+
+                String data = "{ \"accessToken\" : \"" + accessToken + "\" }";
+                logger.info("token 확인>>>>>>"+data);
 
                 // 토큰 확인
-                String url = "http://localhost:8082/v1/api/auth/test";
-                if(!httpConnection(url,tokenString).equals("200")) {
+                String url = "https://webhook.site/1840c760-d74f-4696-8fac-bfb7a185925c";
+                //String url = "http://localhost:8082/v1/api/auth/test";
+
+                if(!httpConnection(url,data).equals("ROLE_MANAGER")) {
                     return handleUnAuthorized(exchange);
                 }
             }
 
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
                 if (config.isPostLogger()) {
-                    logger.info("AuthFilter End>>>>>>" + exchange.getResponse());
+                    logger.info("ManagerFilter End>>>>>>" + exchange.getResponse());
                 }
             }));
         });
@@ -70,15 +80,20 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
     public static String httpConnection(String UrlData, String ParamData) {
 
         //http 요청 시 필요한 url 주소를 변수 선언
-        String totalUrl;
+        String totalUrl = "";
         totalUrl = UrlData.trim().toString();
 
         //http 통신을 하기위한 객체 선언 실시
         URL url = null;
         HttpURLConnection conn = null;
 
+        //http 통신 요청 후 응답 받은 데이터를 담기 위한 변수
+        String responseData = "";
+        BufferedReader br = null;
+        StringBuffer sb = null;
+
         //메소드 호출 결과값을 반환하기 위한 변수
-        String result = "";
+        String returnData = "";
 
         try {
             //파라미터로 들어온 url을 사용해 connection 실시
@@ -89,7 +104,15 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-Type", "application/json; utf-8");
             conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("Cookie", ParamData);
+            conn.setDoOutput(true); //OutputStream을 사용해서 post body 데이터 전송
+            try (OutputStream os = conn.getOutputStream()){
+                byte request_data[] = ParamData.getBytes("utf-8");
+                os.write(request_data);
+                os.close();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
 
             //http 요청 실시
             conn.connect();
@@ -99,21 +122,36 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
             System.out.println("http 요청 데이터 : "+ParamData);
             System.out.println("");
 
-            //http 요청 후 응답
-            result = conn.getResponseMessage();
+            //http 요청 후 응답 받은 데이터를 버퍼에 쌓는다
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            sb = new StringBuffer();
+            while ((responseData = br.readLine()) != null) {
+                sb.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
+            }
+
+            //메소드 호출 완료 시 반환하는 변수에 버퍼 데이터 삽입 실시
+            returnData = sb.toString();
 
             //http 요청 응답 코드 확인 실시
             String responseCode = String.valueOf(conn.getResponseCode());
-            result = responseCode;
             System.out.println("http 응답 코드 : "+responseCode);
-            System.out.println("http 응답 데이터 : "+result);
+            System.out.println("http 응답 데이터 : "+returnData);
 
         } catch (IOException e) {   // 서버 연결 끊겼을 때
             e.printStackTrace();
             logger.error("error>>>>" + e);
+        } finally {
+            //http 요청 및 응답 완료 후 BufferedReader를 닫아줍니다
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        return result;
+        return returnData;
     }
 
     @Data
