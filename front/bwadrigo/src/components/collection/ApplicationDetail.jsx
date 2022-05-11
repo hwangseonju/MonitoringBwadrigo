d023b6c8ae7f71078b13ddaa0cac50a97607f004
hwangseonju <tjswn0425@naver.com>
2022-05-11 10:46:47 +0900

    [S06P31S104-208] HOTFIX: modified for eslint
    S06P31S104-208 #done #comment eslint에 맞게 수정

--- front/bwadrigo/src/components/collection/ApplicationDetail.jsx
+++ front/bwadrigo/src/components/collection/ApplicationDetail.jsx
@@ -1,92 +1,92 @@
-import { useState, useEffect, useCallback } from 'react';
-import {Button,ListGroup} from 'react-bootstrap';
-import axios from 'axios';
+import { useState, useEffect, useCallback } from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import axios from "axios";
 
-function ApplicationDetail(){
-    const [address, setAddress] = useState("test");
-    useEffect(()=>{
-        axios.get("/v1/api/member")
-        .then((res)=>{
-            console.log(res);
-            if(res.data.memberAddress){
-                setAddress(res.data.memberAddress);
-            }
-        })
-    },[]);
-    const [collectList, setCollectList] = useState([])
-    useEffect(()=>{
-        axios.get("/v1/api/order/collect/check").then((res)=>{
-            if(res.status == 204){
-                window.location.replace("/applicationResult")
-            }
-            console.log(res)
-            let collectList = res.data
-            for(let i=0; i<collectList.length; i++){
-                console.log(collectList[i].collecttype)
-                switch(collectList[i].collecttype){
-                    case "bedding" : 
-                        collectList[i].collecttype = "이불" 
-                        break
-                    case "wash" : 
-                        collectList[i].collecttype = "생활빨래" 
-                        break
-                    case "shirts" : 
-                        collectList[i].collecttype = "셔츠" 
-                        break
-                    case "cleaning" : 
-                        collectList[i].collecttype = "개별 클리닝" 
-                        break
-                }
-            }
-            setCollectList(collectList)
-        })
-    },[])
-    const collectWithdraw = useCallback(async(e)=>{
-        console.log(e.target.name)
-        await axios({
-            method : "delete",
-            url : `/v1/api/order/${e.target.name}`
-        }).then((res)=>{
-            alert("철회 신청 되어습니다.")
-            location.reload();
-        })
-        // console.log(e.target.name)
-    })
-    const allWithdraw = useCallback(async(e)=>{
-        for(let collect of collectList){
-            await axios({
-                method : "delete",
-                url : `/v1/api/order/${collect.collectId}`
-            })  
+function ApplicationDetail() {
+  const [address, setAddress] = useState("test");
+  useEffect(() => {
+    axios.get("/v1/api/member").then((res) => {
+      console.log(res);
+      if (res.data.memberAddress) {
+        setAddress(res.data.memberAddress);
+      }
+    });
+  }, []);
+  const [collectList, setCollectList] = useState([]);
+  useEffect(() => {
+    axios.get("/v1/api/order/collect/check").then((res) => {
+      if (res.status == 204) {
+        window.location.replace("/applicationResult");
+      }
+      console.log(res);
+      let collectList = res.data;
+      for (let i = 0; i < collectList.length; i++) {
+        console.log(collectList[i].collecttype);
+        switch (collectList[i].collecttype) {
+          case "bedding":
+            collectList[i].collecttype = "이불";
+            break;
+          case "wash":
+            collectList[i].collecttype = "생활빨래";
+            break;
+          case "shirts":
+            collectList[i].collecttype = "셔츠";
+            break;
+          case "cleaning":
+            collectList[i].collecttype = "개별 클리닝";
+            break;
         }
-        location.reload();
-    })
-    return(
-        <div>
-            <h1>수거 신청 정보</h1>
-            
-            {collectList.map((collect) => (
-                <ListGroup key={collect.collectId} horizontal='xxl' as="ol">
-                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start" >
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">세탁물 종류 : {
-                            collect.collecttype
-                        }</div>
-                        수거 요청일: {collect.collectRequestDate.split("T")[0]}
-                    </div>
-                    <Button bg="primary" name={collect.collectId} onClick={collectWithdraw}>
-                        수거 철회
-                    </Button>
-                    </ListGroup.Item>
-                </ListGroup>
-            ))}
-            <h2>수거/배송 주소</h2>
-            <p>{address}</p>
-            <Button onClick={allWithdraw} variant='danger' size='lg'>수거 전체 철회</Button>
-            <Button href='/applicationResult' variant='success' size='lg'>닫기</Button>
-        </div>
-        
-    )
+      }
+      setCollectList(collectList);
+    });
+  }, []);
+  const collectWithdraw = useCallback(async (e) => {
+    console.log(e.target.name);
+    await axios({
+      method: "delete",
+      url: `/v1/api/order/${e.target.name}`,
+    }).then((res) => {
+      alert("철회 신청 되어습니다.");
+      window.location.reload();
+    });
+    // console.log(e.target.name)
+  });
+  const allWithdraw = useCallback(async (e) => {
+    for (let collect of collectList) {
+      await axios({
+        method: "delete",
+        url: `/v1/api/order/${collect.collectId}`,
+      });
+    }
+    window.location.reload();
+  });
+  return (
+    <div>
+      <h1>수거 신청 정보</h1>
+
+      {collectList.map((collect) => (
+        <ListGroup key={collect.collectId} horizontal="xxl" as="ol">
+          <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">세탁물 종류 : {collect.collecttype}</div>
+              수거 요청일: {collect.collectRequestDate.split("T")[0]}
+            </div>
+            <Button bg="primary" name={collect.collectId} onClick={collectWithdraw}>
+              수거 철회
+            </Button>
+          </ListGroup.Item>
+        </ListGroup>
+      ))}
+      <h2>수거/배송 주소</h2>
+      <p>{address}</p>
+      <Button onClick={allWithdraw} variant="danger" size="lg">
+        수거 전체 철회
+      </Button>
+      <Button href="/applicationResult" variant="success" size="lg">
+        닫기
+      </Button>
+    </div>
+  );
 }
 
 export default ApplicationDetail;
--- front/bwadrigo/src/components/plans/ApplyPlanDetail.jsx
+++ front/bwadrigo/src/components/plans/ApplyPlanDetail.jsx
@@ -1,86 +1,92 @@
 import { useNavigate, useParams } from "react-router-dom";
-import {Card,Button,ListGroup, Table} from 'react-bootstrap';
+import { Card, Button, ListGroup, Table } from "react-bootstrap";
 import MonthPlan from "../../month_plan_data.json";
 import { useCallback, useState } from "react";
 import { async } from "q";
 import axios from "axios";
-function ApplyPlanDetail(){
-    // 객체로 넘어가는게 아니라 id 값만 넘어가게 구현했습니다
-    // detail 부분은 id값으로 axios로 가져오면 되지않을까요..ㅎㅎ......히희..
-    
-    const detail_id = useParams();
-    const [data, setDetail] = useState(MonthPlan[detail_id.applyid-1])
-    // console.log(MonthPlan[detail_id.applyid-1])
-    const navigate = useNavigate();
-    const applyService = useCallback(async(e) => {
-        await axios({
-            method:"post",
-            url : "/v1/api/plan",
-            data : {
-                "monthPlanId" : detail_id.applyid
-            }
-        }).then((res)=>
-            {console.log(res)
-            alert("요금제 신청이 성공했습니다.")
-            window.location.href = "/"}
-        ).catch(e => {
-            if(e.response.status == 401){
-                alert("로그인 후 이용가능합니다.")
-                navigate("/login")
-            }
-            if(e.response.status == 400){
-                if(confirm("이미 사용중인 요금제가 있습니다. 변경신청을 하시겠습니까?")){
-                    axios({
-                        method : 'put',
-                        url : "/v1/api/plan",
-                        data : {
-                            "monthPlanId" : detail_id.applyid
-                        }
-                    }).then(res => {
-                        alert("변경요청이 완료 되었습니다.")
-                        navigate("/");
-                    })
-                }
-            }
-        })
+function ApplyPlanDetail() {
+  // 객체로 넘어가는게 아니라 id 값만 넘어가게 구현했습니다
+  // detail 부분은 id값으로 axios로 가져오면 되지않을까요..ㅎㅎ......히희..
+
+  const detail_id = useParams();
+  const [data, setDetail] = useState(MonthPlan[detail_id.applyid - 1]);
+  // console.log(MonthPlan[detail_id.applyid-1])
+  const navigate = useNavigate();
+  const applyService = useCallback(async (e) => {
+    await axios({
+      method: "post",
+      url: "/v1/api/plan",
+      data: {
+        monthPlanId: detail_id.applyid,
+      },
     })
-    return (
-        <div>
-            {/* <div>{detail_id.applyid}</div> */}
-            <Card className="text-center">
-                <Card.Header> {data.month_plan_name} 요금제</Card.Header>
-                <Card.Body>
-                    <Card.Title>가격 : {data.month_plan_price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}\</Card.Title>
-                    <Card.Text>
-                        <ListGroup variant="flush">
-                            <div>상품구성</div>
-                            <Table style={{ textAlign: "center", fontSize: "12px" }}>
-                                <thead>
-                                <tr>
-                                    {data.month_plan_wash_count !== 0 ? <th>생활빨래</th> : <th style={{ color: "#cfcfcf" }}>생활빨래</th>}
-                                    {data.month_plan_shirt_count !== 0 ? <th>와이셔츠</th> : <th style={{ color: "#cfcfcf" }}>와이셔츠</th>}
-                                    {data.month_plan_cleaning_count !== 0 ? <th>개별클리닝</th> : <th style={{ color: "#cfcfcf" }}>개별클리닝</th>}
-                                    {data.month_plan_bedding_count !== 0 ? <th>이불</th> : <th style={{ color: "#cfcfcf" }}>이불</th>}
-                                    {data.month_plan_delivery_count !== 0 ? <th>수거배송</th> : <th style={{ color: "#cfcfcf" }}>수거배송</th>}
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    {data.month_plan_wash_count !== 0 ? <td>{data.month_plan_wash_count}개</td> : <td style={{ color: "#cfcfcf" }}>0개</td>}
-                                    {data.month_plan_shirt_count !== 0 ? <td>{data.month_plan_shirt_count}장</td> : <td style={{ color: "#cfcfcf" }}>0장</td>}
-                                    {data.month_plan_cleaning_count !== 0 ? <td>{data.month_plan_cleaning_count}장</td> : <td style={{ color: "#cfcfcf" }}>0장</td>}
-                                    {data.month_plan_bedding_count !== 0 ? <td>{data.month_plan_bedding_count}개</td> : <td style={{ color: "#cfcfcf" }}>0개</td>}
-                                    {data.month_plan_delivery_count !== 0 ? <td>{data.month_plan_delivery_count}회</td> : <td style={{ color: "#cfcfcf" }}>0회</td>}
-                                </tr>
-                                </tbody>
-                            </Table>
-                        </ListGroup>
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer><Button variant="primary" onClick={applyService}>신청하기</Button></Card.Footer>
-            </Card>
-        </div>
-    )
+      .then((res) => {
+        console.log(res);
+        alert("요금제 신청이 성공했습니다.");
+        window.location.href = "/";
+      })
+      .catch((e) => {
+        if (e.response.status == 401) {
+          alert("로그인 후 이용가능합니다.");
+          navigate("/login");
+        }
+        if (e.response.status == 400) {
+          if (window.confirm("이미 사용중인 요금제가 있습니다. 변경신청을 하시겠습니까?")) {
+            axios({
+              method: "put",
+              url: "/v1/api/plan",
+              data: {
+                monthPlanId: detail_id.applyid,
+              },
+            }).then((res) => {
+              alert("변경요청이 완료 되었습니다.");
+              navigate("/");
+            });
+          }
+        }
+      });
+  });
+  return (
+    <div>
+      {/* <div>{detail_id.applyid}</div> */}
+      <Card className="text-center">
+        <Card.Header> {data.month_plan_name} 요금제</Card.Header>
+        <Card.Body>
+          <Card.Title>가격 : {data.month_plan_price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}\</Card.Title>
+          <Card.Text>
+            <ListGroup variant="flush">
+              <div>상품구성</div>
+              <Table style={{ textAlign: "center", fontSize: "12px" }}>
+                <thead>
+                  <tr>
+                    {data.month_plan_wash_count !== 0 ? <th>생활빨래</th> : <th style={{ color: "#cfcfcf" }}>생활빨래</th>}
+                    {data.month_plan_shirt_count !== 0 ? <th>와이셔츠</th> : <th style={{ color: "#cfcfcf" }}>와이셔츠</th>}
+                    {data.month_plan_cleaning_count !== 0 ? <th>개별클리닝</th> : <th style={{ color: "#cfcfcf" }}>개별클리닝</th>}
+                    {data.month_plan_bedding_count !== 0 ? <th>이불</th> : <th style={{ color: "#cfcfcf" }}>이불</th>}
+                    {data.month_plan_delivery_count !== 0 ? <th>수거배송</th> : <th style={{ color: "#cfcfcf" }}>수거배송</th>}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {data.month_plan_wash_count !== 0 ? <td>{data.month_plan_wash_count}개</td> : <td style={{ color: "#cfcfcf" }}>0개</td>}
+                    {data.month_plan_shirt_count !== 0 ? <td>{data.month_plan_shirt_count}장</td> : <td style={{ color: "#cfcfcf" }}>0장</td>}
+                    {data.month_plan_cleaning_count !== 0 ? <td>{data.month_plan_cleaning_count}장</td> : <td style={{ color: "#cfcfcf" }}>0장</td>}
+                    {data.month_plan_bedding_count !== 0 ? <td>{data.month_plan_bedding_count}개</td> : <td style={{ color: "#cfcfcf" }}>0개</td>}
+                    {data.month_plan_delivery_count !== 0 ? <td>{data.month_plan_delivery_count}회</td> : <td style={{ color: "#cfcfcf" }}>0회</td>}
+                  </tr>
+                </tbody>
+              </Table>
+            </ListGroup>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="primary" onClick={applyService}>
+            신청하기
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 }
 
 export default ApplyPlanDetail;
