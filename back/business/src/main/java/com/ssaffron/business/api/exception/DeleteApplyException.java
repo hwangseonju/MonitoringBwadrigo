0ec5f4f174ba4fd04a41bc3a837f78f75083e71c
dodssockii <dodssockii@gmail.com>
2022-05-13 17:59:21 +0900

    [S06P31S104-240] Refactor: update code refactoring
    - 예외처리 미사용 부분 정리 및 설명 추가
    - PlanService orElseThrow를 이용하여 예외 보내기
    - PlanController, PlanService 사용하지 않는 코드 삭제
    - BucketContoller traffic 제어 주석
    - MemberEntity applyForEntity를 applyEntity로 수정

--- back/business/src/main/java/com/ssaffron/business/api/exception/DeleteApplyException.java
+++ back/business/src/main/java/com/ssaffron/business/api/exception/DeleteApplyException.java
@@ -5,18 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DeleteApplyException extends RuntimeException {
 
+    // 사용하지 않는 것 같음 모든 테스트 후 사용되지 않으면 삭제 예정
     private ErrorCode errorCode;
-
-    public DeleteApplyException(String messgae, ErrorCode errorCode){
-        super(messgae);
-        this.errorCode = errorCode;
-        log.info("님께서 신청한 서비스가 없습니다.");
-    }
-
-    public DeleteApplyException(ErrorCode errorCode){
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
 
     public DeleteApplyException(String messgae){
         super(messgae);
@@ -26,8 +16,5 @@ public class DeleteApplyException extends RuntimeException {
     public ErrorCode getErrorCode(){
         return this.errorCode;
     }
-//    public DeleteApplyException(String memberName){
-//        super(memberName + "님께서 신청한 요금제가 없습니다.");
-//        log.info("{}님께서 신청한 서비스가 없습니다.", memberName);
-//    }
+
 }
--- back/business/src/main/java/com/ssaffron/business/api/exception/NotFoundAddressException.java
+++ back/business/src/main/java/com/ssaffron/business/api/exception/NotFoundAddressException.java
@@ -7,18 +7,6 @@ public class NotFoundAddressException extends NullPointerException{
 
     private ErrorCode errorCode;
 
-    public NotFoundAddressException(String message, ErrorCode errorCode){
-        super(message);
-        this.errorCode = errorCode;
-        log.info("주소가 입력되지 않아 서비스를 이용하실 수 없습니다.");
-    }
-
-    // 에러 클래스에서 에러 메시지 전달
-    public NotFoundAddressException(ErrorCode errorCode){
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-
     public NotFoundAddressException(String message){
         super(message);
         log.info("주소가 입력되지 않아 서비스를 이용하실 수 없습니다.");
@@ -29,9 +17,4 @@ public class NotFoundAddressException extends NullPointerException{
         return this.errorCode;
     }
 
-
-//    public NotFoundAddressException(){
-//        super("주소가 입력되지 않아 서비스를 이용하실 수 없습니다.");
-//        log.info("주소가 입력되지 않아 서비스를 이용하실 수 없습니다.");
-//    }
 }
