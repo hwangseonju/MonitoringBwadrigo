d322594dcb52374b5adf314f92650b686af1eaa7
장정훈 <chlj1101@naver.com>
2022-05-15 20:06:28 +0000

    Update setupProxy.js

--- front/bwadrigo/src/setupProxy.js
+++ front/bwadrigo/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/v1/api',{
-      target: 'http://k6s1041.p.ssafy.io:8083',
+      target: 'http://k6s104.p.ssafy.io:8082',
       changeOrigin: true
     })
   );
