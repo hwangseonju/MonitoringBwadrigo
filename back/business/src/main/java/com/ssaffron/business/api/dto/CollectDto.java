514774b6c38ff7865acc0d1f553dde8adb1bd8ae
SSAFY <chlj1101@gmail.com>
2022-05-16 14:48:08 +0900

    [S06P31S104-271] Fix: update CollectDto
    - CollectDto 안에 ã있ë  collecttype -> collectType으로 수정
    - 서비스에ì  멘í토링 ê기반 변경

--- back/business/src/main/java/com/ssaffron/business/api/dto/CollectDto.java
+++ back/business/src/main/java/com/ssaffron/business/api/dto/CollectDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 public class CollectDto {
     private long collectId;
-    private String collecttype;
+    private String collectType;
     private LocalDateTime collectRequestDate;
     private LocalDateTime collectWithdrawDate;
 
