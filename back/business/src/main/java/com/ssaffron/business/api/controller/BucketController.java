58bad32541828e719d6f76f05c99ac7b59153eea
dodssockii <dodssockii@gmail.com>
2022-05-18 14:22:13 +0900

    [S06P31S104-265] Feat: modify bucket4j and slack
    bucket4j error code 추가 및 슬랙 연동 주석

--- back/business/src/main/java/com/ssaffron/business/api/controller/BucketController.java
+++ back/business/src/main/java/com/ssaffron/business/api/controller/BucketController.java
@@ -1,6 +1,12 @@
 package com.ssaffron.business.api.controller;
 
-import com.ssaffron.business.api.entity.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssaffron.business.api.exception.ErrorCode;
+import com.ssaffron.business.api.exception.ErrorResponse;
+import com.ssaffron.business.api.exception.NotFoundAddressException;
+import com.ssaffron.business.api.exception.TooManyException;
+import com.ssaffron.business.api.success.SuccessCode;
+import com.ssaffron.business.api.success.SuccessHandler;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
@@ -9,6 +15,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.Duration;
 
 @Slf4j
@@ -16,9 +26,12 @@ import java.time.Duration;
 @RequestMapping(value = "/v1/api/bucket")
 public class BucketController {
 
+    private final SuccessHandler successHandler;
+
     private Bucket bucket;
 
-    public BucketController() {
+    public BucketController(SuccessHandler successHandler) {
+        this.successHandler = successHandler;
         //10분에 10개의 요청을 처리할 수 있는 Bucket 생성
         Bandwidth limit = Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(10)));
         this.bucket = Bucket.builder()
@@ -27,14 +40,25 @@ public class BucketController {
     }
 
     @GetMapping("")
-    public ResponseEntity<ResponseEntity> bucketTest() {
+    public ResponseEntity bucketTest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
 
         if (bucket.tryConsume(1)) {
-            log.info("Success");
+//            successHandler.sendSuccessLog(SuccessCode.TOO_MANY,"GET v1/api/bucket");
             return ResponseEntity.status(HttpStatus.OK).build();
+        }else {
+            throw new TooManyException("Too Many Requests");
         }
 
-        log.error("TOO MANY REQUEST");
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+//        httpServletResponse.setStatus(429);
+//        httpServletResponse.setContentType("application/json;charset=utf-8");
+//        PrintWriter out = httpServletResponse.getWriter();
+//        ErrorResponse response = ErrorResponse.of(ErrorCode.TOO_MANY_REQEUSTS);
+//        response.setDetail("너무 많은 요청입니다.");
+//        String jsonResponse = objectMapper.writeValueAsString(response);
+//        out.print(jsonResponse);
+//        log.error("TOO MANY REQUEST");
+//        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
