7ef3546f4c776b644c65352da8ea9e38cd806015
hwangseonju <tjswn0425@naver.com>
2022-05-18 16:26:21 +0900

    [S06P31S104-273] Feat: add usePlan CSS and update applyPlan data
    S06P31S104-273 #done #comment 요금제 관련 화면 CSS 작업
    추가사항으로 요금제 변경화면에 사용중인 요금제 값이 필요하여 수정함

--- front/bwadrigo/src/components/usePlan/StartTermination.jsx
+++ front/bwadrigo/src/components/usePlan/StartTermination.jsx
@@ -1,4 +1,5 @@
 import { Button } from "react-bootstrap";
+import styles from "./UsePlan.css";
 
 function StartTermination() {
   const goSurvey = () => {
@@ -11,21 +12,33 @@ function StartTermination() {
 
   return (
     <div>
-      <h3>서비스 해지</h3>
-      <div>
-        <b>꼭 확인해주세요.</b>
+      <h3 className="text-center Title">서비스 해지</h3>
+      <hr />
+      <div className="full-content">
+        <div>
+          <b style={{ fontSize: "20px", paddingLeft: "10px" }}>꼭 확인해주세요.</b>
+        </div>
+        <br />
+        <ul className="notice">
+          <li>
+            월정액 요금제에 포함된 미사용 품목은 환불되지
+            <br />
+            않습니다.
+          </li>
+          <li>
+            초과 사용 품목이 있는 경우, 해당 금액에 대한
+            <br />
+            결제가 진행됩니다.
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>월정액 요금제에 포함된 미사용 품목은 환불되지않습니다.</li>
-        <li>초과 사용 품목이 있는 경우, 해당 금액에 대한 결제가 진행됩니다.</li>
-      </ul>
       <div>
-        <Button variant="primary" onClick={goCancle}>
+        <button className="btnPlan" onClick={goCancle}>
           서비스 유지하기
-        </Button>
-        <Button variant="light" onClick={goSurvey}>
+        </button>
+        <button className="btnCancle" onClick={goSurvey}>
           해지하기
-        </Button>
+        </button>
       </div>
     </div>
   );
